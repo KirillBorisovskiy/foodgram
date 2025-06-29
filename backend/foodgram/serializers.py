59cf69d94a3e18db785d22f709fb8f556d5b1e1e
KirillBorisovskiy <kirilllonely@gmail.com>
2025-06-29 16:30:46 +0300

    fix image

--- backend/foodgram/serializers.py
+++ backend/foodgram/serializers.py
@@ -11,7 +11,7 @@ from foodgram.models import Ingredient, IngredientRecipe, Recipe, Tag, User
 
 
 class AvatarSerializer(serializers.ModelSerializer):
-    avatar = Base64ImageField(required=True, allow_null=True)
+    avatar = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
@@ -54,7 +54,7 @@ class UserSerializer(serializers.ModelSerializer):
         write_only=True,
         validators=[validate_password],
     )
-    avatar = Base64ImageField(required=True)
+    avatar = Base64ImageField(required=False)
 
     class Meta:
         model = User
@@ -145,7 +145,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     ingredients = serializers.SerializerMethodField()
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
-    image = Base64ImageField(required=True)
+    image = Base64ImageField(required=False)
 
     class Meta:
         model = Recipe
